04381cf718f7d90fa97977d78088b6767a28e8fb
Farid Vatani <faridvatani@gmail.com>
2024-08-07 23:57:07 +0330

    feat: Implement reusable dialog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,78 @@
 "use client";
+import { useState } from "react";
 import Button from "@/components/Button";
+import Dialog from "@/components/Dialog";
 import TextSpan from "@/components/TextSpan";
 import { Wrapper } from "@/components/Wrapper";
 
 export default function Home() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleOpenDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start py-24 px-52">
-      <Wrapper title="My Portfolio" className="w-full bg-white">
-        <TextSpan>
-          Hi, I&apos;m a software developer. I&apos;m passionate about building
-          web applications and solving problems. I&apos;m currently learning
-          about TypeScript, React, and Node.js.
-        </TextSpan>
-        <TextSpan>
-          I&apos;m also interested in learning more about cloud computing and
-          serverless technologies.
-        </TextSpan>
-        <TextSpan>
-          I&apos;m currently looking for opportunities to work as a software
-          developer.
-        </TextSpan>
-
-        <div className="mt-10">
+    <>
+      <Dialog
+        isOpen={isDialogOpen}
+        onClose={handleCloseDialog}
+        closeOnOverlayClick
+        rounded
+      >
+        <p>Are you sure you want to proceed?</p>
+        <Button onClick={handleCloseDialog}>Close</Button>
+      </Dialog>
+      <main className="flex min-h-screen flex-col items-center justify-start py-24 px-52">
+        <Wrapper title="My Portfolio" className="w-full bg-white">
           <TextSpan>
-            Here are some of the technologies I&apos;ve been working with
-            recently:
+            Hi, I&apos;m a software developer. I&apos;m passionate about
+            building web applications and solving problems. I&apos;m currently
+            learning about TypeScript, React, and Node.js.
           </TextSpan>
-          <ul>
-            <li>JavaScript (ES6+)</li>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>PostgreSQL</li>
-            <li>MySQL</li>
-            <li>MongoDB</li>
-            <li>Git</li>
-          </ul>
-        </div>
-
-        <div className="mt-10">
           <TextSpan>
-            Here are some of the technologies I&apos;m currently learning:
+            I&apos;m also interested in learning more about cloud computing and
+            serverless technologies.
           </TextSpan>
-          <ul>
-            <li>TypeScript</li>
-            <li>GraphQL</li>
-            <li>Prisma</li>
-            <li>Serverless</li>
-            <li>AWS</li>
-            <li>Docker</li>
-            <li>Kubernetes</li>
-          </ul>
-        </div>
-
-        <div className="mt-10 w-fit flex gap-4 justify-center">
-          <Button variant="primary">Contact me</Button>
-          <Button>View projects</Button>
-          <Button>Resume</Button>
-        </div>
-      </Wrapper>
-    </main>
+          <TextSpan>
+            I&apos;m currently looking for opportunities to work as a software
+            developer.
+          </TextSpan>
+          <div className="mt-10">
+            <TextSpan>
+              Here are some of the technologies I&apos;ve been working with
+              recently:
+            </TextSpan>
+            <ul>
+              <li>JavaScript (ES6+)</li>
+              <li>React</li>
+              <li>Next.js</li>
+              <li>Node.js</li>
+              <li>Express</li>
+              <li>PostgreSQL</li>
+              <li>MySQL</li>
+              <li>MongoDB</li>
+              <li>Git</li>
+            </ul>
+          </div>
+          <div className="mt-10">
+            <TextSpan>
+              Here are some of the technologies I&apos;m currently learning:
+            </TextSpan>
+            <ul>
+              <li>TypeScript</li>
+              <li>GraphQL</li>
+              <li>Prisma</li>
+            </ul>
+          </div>
+          <div className="mt-10 w-fit flex gap-4 justify-center">
+            <Button onClick={handleOpenDialog}>Resume</Button>
+          </div>
+        </Wrapper>
+      </main>
+    </>
   );
 }
---
+++ src/components/Dialog.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+interface DialogProps {
+  isOpen: boolean;
+  className?: string;
+  dark?: boolean;
+  rounded?: boolean;
+  children?: React.ReactNode;
+  onClose?: () => void;
+  closeOnOverlayClick?: boolean;
+}
+
+const Dialog: React.FC<DialogProps> = ({
+  isOpen,
+  className = "",
+  dark,
+  rounded,
+  children,
+  onClose = () => {},
+  closeOnOverlayClick = false,
+}) => {
+  if (!isOpen) return null;
+
+  const handleOverlayClick = () => {
+    if (closeOnOverlayClick) {
+      onClose();
+    }
+  };
+
+  const handleDialogClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div
+        className="fixed inset-0 bg-black opacity-50"
+        onClick={handleOverlayClick}
+      ></div>
+      <dialog
+        className={`relative z-10 nes-dialog backdrop-blur ${className} ${
+          dark ? "is-dark" : ""
+        } ${rounded ? "is-rounded" : ""}`}
+        onClick={handleDialogClick}
+        open
+        aria-labelledby="dialog-title"
+        aria-describedby="dialog-message"
+      >
+        {children}
+      </dialog>
+    </div>
+  );
+};
+
+export default Dialog;
